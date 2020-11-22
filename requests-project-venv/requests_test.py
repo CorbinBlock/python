b48f68af96af44cb78909808b872af2e673e5b21
CorbinBlock <williamcorbinblock@gmail.com>
2020-11-22 17:21:05 -0600

    python repository 2020-11-21 commit (requests and request.html libraries)

--- requests-project-venv/requests_test.py
+++ requests-project-venv/requests_test.py
@@ -1,18 +1,23 @@
 import requests
-r = requests.get('https://www.ozk.com/login/', auth=('user', 'pass'))
-r.status_code
-r.text
 
 #write requests object to file 
 #source - https://stackoverflow.com/questions/14114729/save-a-large-file-using-the-python-requests-library
 
-# Throw an error for bad status codes
-r.raise_for_status()
-
-with open('output.html', 'wb') as handle:
-    for block in r.iter_content(1024):
-            handle.write(block)
-
 #print the attributes and methods associated with an object. in this example we are examining the response object from the request library
 
-print(dir(r))
+
+
+
+response = requests.get('https://ozk.com/login/', stream=True)
+
+# Throw an error for bad status codes
+response.raise_for_status()
+
+with open('output.html', 'wb') as handle:
+        for block in response.iter_content(1024):
+                    handle.write(block)
+
+response.text
+
+
+print(dir(response))
