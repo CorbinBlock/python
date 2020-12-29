b21aa6ad72bdd40665625a40cabdefa2bfa4b938
Corbin Block <williamcorbinblock@gmail.com>
2020-12-29 10:28:10 -0600

    startup program subprocess update 2020-12-28
    startup program subprocess update 2020-12-28

--- file_management/venv/Scripts/gmail_api.py
+++ file_management/venv/Scripts/gmail_api.py
@@ -2,6 +2,6 @@ import ezgmail
 import os
 
 filename = "credentials.json"
-jsonpath = os.path.join("C:\\Users\\Corbin\\PycharmProjects\\file_mgmt\\venv\\Scripts", filename)
+jsonpath = os.path.join("C:\\Users\\Corbin\\PycharmProjects\\python\\file_management\\venv\\Scripts", filename)
 os.chdir(jsonpath)
 ezgmail.init()
--- file_management/venv/Scripts/open_startup_programs.py
+++ file_management/venv/Scripts/open_startup_programs.py
@@ -7,21 +7,24 @@ def open_programs():
     driveletter = "C:"
     basepath = "\\Program Files\\"
     basepath86 = "\\Program Files (x86)\\"
+    # shortcutpath = "\\ProgramData\\Microsoft\\Windows\\StartMenu\\Programs"
     chrome = os.path.join(driveletter, basepath, "Google\\Chrome\\Application\\chrome.exe")
     notepadplusplus = os.path.join(driveletter, basepath86, "Notepad++\\notepad++.exe")
     vscode = os.path.join(driveletter, basepath, "Microsoft VS Code\\bin\\code.cmd")
     keepass = os.path.join(driveletter, basepath86, "KeePass Password Safe 2\\KeePass.exe")
+    # outlook = os.path.join(driveletter, shortcutpath,"Outlook.lnk")
+    # TODO - fix Outlook
     outlook = os.path.join(driveletter, basepath, "Microsoft Office\\root\\Office16\\OUTLOOK.EXE")
     pycharm = os.path.join(driveletter, basepath, "JetBrains\\PyCharm Community Edition 2020.3\\bin\\pycharm64.exe")
     onenote = os.path.join(driveletter, basepath, "Microsoft Office\\root\\Office16\\ONENOTE.EXE")
     winSCP = os.path.join(driveletter, basepath86, "WinSCP\\WinSCP.exe")
-    malwarebytes = os.path.join(driveletter, basepath, "Malwarebytes\\Anti-Malware\\mbam.exe")
+    sophos = os.path.join(driveletter, basepath86, "Sophos\\Sophos Home\\SophosUI.exe")
     spotify = os.path.join(driveletter, "\\Users\\Corbin\\AppData\\Roaming\\Spotify\\Spotify.exe")
     githubdesktop = os.path.join(driveletter, "\\Users\\Corbin\\AppData\\Local\\GitHubDesktop\\GitHubDesktop.exe")
 
 
     # arrange into list and store length
-    list = [vscode, notepadplusplus, keepass, outlook, pycharm, chrome, onenote, winSCP, malwarebytes, spotify, githubdesktop]
+    list = [vscode, notepadplusplus, keepass, outlook, chrome, onenote, winSCP, sophos, spotify, githubdesktop] # pycharm
     length = len(list)
     # iterate through list to open all programs
     for i in range(length):
