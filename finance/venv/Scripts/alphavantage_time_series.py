bbf5a85791af4ae0d180795e01c4bd220400a8a3
Corbin Block <williamcorbinblock@gmail.com>
2020-12-31 17:43:42 -0600

    call alphavantage api - json string to python dict - 2020-12-31
    call alphavantage api - json string to python dict - 2020-12-31

--- finance/venv/Scripts/alphavantage_time_series.py
+++ finance/venv/Scripts/alphavantage_time_series.py
@@ -1,19 +1,36 @@
 import requests
 import alphavantage
 import csv
-import pandas
+# import pandas
+import json
+import os
 
+# API key stored in environment variable
+
+api_key = os.getenv('ALPHAVANTAGE_API_KEY')
 API_URL = "https://www.alphavantage.co/query"
 
 data = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
-    "symbol": "GOEV",
+    "symbol": "PLTR",
     "outputsize": "full",
-    "datatype": "csv",
-    "apikey": "D46872A04A9M1143LDD",
+    "datatype": "json",
+    "apikey": api_key,
 }
 
 response = requests.get(API_URL, data)
-df = pandas.DataFrame(response)
-print('DataFrame:', df)
-stock_csv_data = df.to_csv('GOEV.csv', index=True)
+
+response.json()
+
+# parse JSON string into python dict
+json_data = json.loads(response.text)
+print(type(json_data))
+
+with open("data_file.json", "w") as write_file:
+    json.dump(json_data, write_file)
+
+# df = pandas.DataFrame(response)
+# print('DataFrame:', df)
+#
+#
+# stock_csv_data = df.to_csv('GOEV.csv', index=True)
