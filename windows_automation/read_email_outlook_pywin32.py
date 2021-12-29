cd448c5c16edd617c4bfd8edcf9a7c0f78ded0b2
root <root@CBLOCKDYFX2X2.pclc0.merkle.local>
2021-12-29 11:02:05 -0600

    initial

--- windows_automation/read_email_outlook_pywin32.py
+++ windows_automation/read_email_outlook_pywin32.py
@@ -1,6 +1,7 @@
-import win32com.client
-import os
 from datetime import datetime, timedelta
+import glob
+import os
+import win32com.client
 
 # email = os.getenv('EMAIL')
 
@@ -14,10 +15,10 @@ inbox = mapi.GetDefaultFolder(6)
 
 messages = inbox.Items
 
-received_dt = datetime.now() - timedelta(days=7)
+received_dt = datetime.now() - timedelta(days=21)
 received_dt = received_dt.strftime('%m/%d/%Y %H:%M %p')
 messages = messages.Restrict("[ReceivedTime] >= '" + received_dt + "'")
-# messages = messages.Restrict("[SenderEmailAddress] = '<email>'")
+# messages = messages.Restrict("[SenderEmailAddress] = 'contact@codeforests.com'")
 # messages = messages.Restrict("[Subject] = 'Sample Report'")
 
 #Let's assume we want to save the email attachment to the below directory
@@ -33,3 +34,16 @@ try:
             print("error when saving the attachment:" + str(e))
 except Exception as e:
     print("error when processing emails messages:" + str(e))
+
+# Delete .png files from attachments (usually junk icons from email)
+# Get a list of all the file paths that ends with .txt from in specified directory
+
+fileList = glob.glob('C:\\tmp\\*.png') + glob.glob('C:\\tmp\\*.jpg') + glob.glob('C:\\tmp\\*.gif')
+
+# Iterate over the list of filepaths & remove each file.
+
+for filePath in fileList:
+    try:
+        os.remove(filePath)
+    except:
+        print("Error while deleting file : ", filePath)
