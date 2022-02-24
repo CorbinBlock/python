08a7ecb2c52c8445b040e991c4acc9b82bc32957
corbinblock <williamcorbinblock@gmail.com>
2022-02-24 13:07:22 -0600

    initial

--- os/reboot.py
+++ os/reboot.py
@@ -2,12 +2,11 @@ import os
 
 # cmd
 # command = "shutdown /r"
+path_dir: str = r"C:\Users\Administrator"
 
 #pwsh
-command = "pwsh.exe -c ls"
+command = "pwsh.exe -c ls {}".format(path_dir)
 
 
 
 os.system(command)
-
-
