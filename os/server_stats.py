594e60df7ead12bc7a084cdb00b254eca92eea51
corbinblock <williamcorbinblock@gmail.com>
2022-03-02 23:24:10 -0600

    initial

--- os/server_stats.py
+++ os/server_stats.py
@@ -2,6 +2,6 @@ import os
 
 shell = "pwsh.exe"
 option = "-c"
-command = "Restart-Computer -ComputerName WIN-01-LENOVO"
+command = "Get-CPUsage; Get-LastBootTime; Get-MemoryUsage"
 command_string = f" {shell} {option} {command}"
 os.system(command_string)
