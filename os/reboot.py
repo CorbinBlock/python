91e6702448a464d1b27caa5dd60f4259d25ef5e9
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-03 00:04:37 -0600

    initial

--- os/reboot.py
+++ os/reboot.py
@@ -6,4 +6,5 @@ option = "-c"
 command = "Restart-Computer -ComputerName "
 arg_one = sys.argv[1]
 command_string = f" {shell} {option} {command} {arg_one}"
+print(command_string)
 os.system(command_string)
