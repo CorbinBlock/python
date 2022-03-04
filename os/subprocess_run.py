8438aeaf4c196f755017b9e4cd4167e7857db42a
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 22:09:22 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -14,27 +14,32 @@ fedora_command = """
 dnf upgrade -y
 cd /git/python && git pull --no-rebase
 """
+option = "-c"
+
+powershell = "pwsh.exe"
+
+shell = "/bin/bash"
 
 wsl_command = 'bash -c "apt-get update && apt-get upgrade -y && cd /git/python && git pull --no-rebase"'
 
 if platform == "linux" or platform == "linux2":
     print(f" {platform} server detected!")
-    output = subprocess.run(["/bin/bash", "-c", command], capture_output=True)
+    output = subprocess.run([shell, option, command], capture_output=True)
     if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
-        print(subprocess.run(["/bin/bash", "-c", debian_command]))
+        print(subprocess.run([shell, option, debian_command]))
     elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
-        print(subprocess.run(["/bin/bash", "-c", debian_command]))
+        print(subprocess.run([shell, option, debian_command]))
     elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
-        print(subprocess.run(["/bin/bash", "-c", debian_command]))
+        print(subprocess.run([shell, option, debian_command]))
     elif "UNIX-01-DEBIAN" in output.stdout.decode('utf-8').split():
-        print(subprocess.run(["/bin/bash", "-c", debian_command]))
+        print(subprocess.run([shell, option, debian_command]))
     elif "UNIX-02-FEDORA" in output.stdout.decode('utf-8').split():
-        print(subprocess.run(["/bin/bash", "-c", fedora_command]))
+        print(subprocess.run([shell, option, fedora_command]))
     else:
         print('hostname not found!')
 elif platform == "win32":
     print(f" {platform} server detected!")
-    output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
+    output = subprocess.run([powershell, option, command], capture_output=True)
     if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
         print(f"{debian_command}")
         print(subprocess.run("wsl -u root"))
