c747ad0852c4fb5e3265a93483dae0dbf848e2aa
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 18:46:53 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -8,6 +8,7 @@ $host_name
 output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
 
 if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
+    print("apt-get update && apt-get upgrade -y && exit")
     print(subprocess.run("wsl -u root"))
 elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
     print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
