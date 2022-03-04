1da91c8cf2325f5ce9c135a60774c5518e3d991a
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 19:09:28 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -1,11 +1,18 @@
 import subprocess
+from sys import platform
 
 command = """
-$host_name = $(hostname)
-$host_name
+hostname
 """
 
-output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
+if platform == "linux" or platform == "linux2":
+    print(f" {platform} server detected!")
+    output = subprocess.run(["/bin/bash", "-c", command], capture_output=True)
+elif platform == "win32":
+    print(f" {platform} server detected!")
+    output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
+else:
+    print(f" {platform} server detected! Please check server.")
 
 if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
     print("apt-get update && apt-get upgrade -y && exit")
