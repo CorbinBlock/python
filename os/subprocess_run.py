8d319ac11614e6f86a1fd14b1913d8e97e1ffc06
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 19:22:48 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -11,15 +11,14 @@ if platform == "linux" or platform == "linux2":
 elif platform == "win32":
     print(f" {platform} server detected!")
     output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
+    if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
+        print("apt-get update && apt-get upgrade -y && exit")
+        print(subprocess.run("wsl -u root"))
+    elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
+        print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
+    elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
+        print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
+    else:
+        print('hostname not found!')
 else:
     print(f" {platform} server detected! Please check server.")
-
-if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
-    print("apt-get update && apt-get upgrade -y && exit")
-    print(subprocess.run("wsl -u root"))
-elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
-    print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
-elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
-    print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
-else:
-    print('hostname not found!')
