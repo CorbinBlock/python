ab507dd05eca0d0e42fe112e1b859385c57f0c66
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 19:43:53 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -8,6 +8,23 @@ hostname
 if platform == "linux" or platform == "linux2":
     print(f" {platform} server detected!")
     output = subprocess.run(["/bin/bash", "-c", command], capture_output=True)
+    if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
+        command = "apt-get update && apt-get upgrade -y"
+        print(subprocess.run(["/bin/bash", "-c", command]))
+    elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
+        command = "apt-get update && apt-get upgrade -y"
+        print(subprocess.run(["/bin/bash", "-c", command]))
+    elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
+        command = "apt-get update && apt-get upgrade -y"
+        print(subprocess.run(["/bin/bash", "-c", command]))
+    elif "UNIX-01-DEBIAN" in output.stdout.decode('utf-8').split():
+        command = "apt-get update && apt-get upgrade -y"
+        print(subprocess.run(["/bin/bash", "-c", command]))
+    elif "UNIX-02-FEDORA" in output.stdout.decode('utf-8').split():
+        command = "dnf upgrade -y"
+        print(subprocess.run(["/bin/bash", "-c", command]))
+    else:
+        print('hostname not found!')
 elif platform == "win32":
     print(f" {platform} server detected!")
     output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
