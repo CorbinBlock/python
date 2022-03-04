bc8b087afe41deb53d25f39d915229ba2f541e64
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 22:13:30 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -25,27 +25,29 @@ wsl_command = 'bash -c "apt-get update && apt-get upgrade -y && cd /git/python &
 if platform == "linux" or platform == "linux2":
     print(f" {platform} server detected!")
     output = subprocess.run([shell, option, command], capture_output=True)
-    if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
+    output_string = output.stdout.decode('utf-8').split()
+    if "WIN-01-LENOVO" in output_string:
         print(subprocess.run([shell, option, debian_command]))
-    elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
+    elif "WIN-02-ASUS" in output_string:
         print(subprocess.run([shell, option, debian_command]))
-    elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
+    elif "CBLOCKDYFX2X2" in output_string:
         print(subprocess.run([shell, option, debian_command]))
-    elif "UNIX-01-DEBIAN" in output.stdout.decode('utf-8').split():
+    elif "UNIX-01-DEBIAN" in output_string:
         print(subprocess.run([shell, option, debian_command]))
-    elif "UNIX-02-FEDORA" in output.stdout.decode('utf-8').split():
+    elif "UNIX-02-FEDORA" in output_string:
         print(subprocess.run([shell, option, fedora_command]))
     else:
         print('hostname not found!')
 elif platform == "win32":
     print(f" {platform} server detected!")
     output = subprocess.run([powershell, option, command], capture_output=True)
-    if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
+    output_string = output.stdout.decode('utf-8').split()
+    if "WIN-01-LENOVO" in output_string:
         print(f"{debian_command}")
         print(subprocess.run("wsl -u root"))
-    elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
+    elif "WIN-02-ASUS" in output_string:
         print(subprocess.run(wsl_command))
-    elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
+    elif "CBLOCKDYFX2X2" in output_string:
         print(subprocess.run(wsl_command))
         # print(subprocess.run(wsl_command))
     else:
