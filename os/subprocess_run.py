65b6421c56409289cb71374357d8ddc38e68afd0
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-03 18:41:06 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -11,5 +11,7 @@ if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
     print(subprocess.run("wsl -u root"))
 elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
     print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
+elif "CBLOCKDYFX2X2" in output.stdout.decode('utf-8').split():
+    print(subprocess.run("wsl apt-get update && apt-get upgrade -y"))
 else:
     print('hostname not found!')
