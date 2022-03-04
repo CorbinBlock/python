8c42c995d300c5e2f172c90961c8a39b1243631d
corbinblock <williamcorbinblock@gmail.com>
2022-03-03 18:08:29 -0600

    initial

--- os/subprocess_run.py
+++ os/subprocess_run.py
@@ -3,20 +3,13 @@ import subprocess
 command = """
 $host_name = $(hostname)
 $host_name
-
 """
 
 output = subprocess.run(["pwsh.exe", "-c", command], capture_output=True)
-# output = subprocess.run(["pwsh.exe", "-c", command], stdout=subprocess.PIPE, text=True)
 
-# print(type(str(output)))
-
-# output == str(output)
-# print(output)
-
-output == output.stdout.decode('utf-8')
 if "WIN-01-LENOVO" in output.stdout.decode('utf-8').split():
-     print(output)
-
-# if output == 'WIN-01-LENOVO':
-#     print(output)
+    print(subprocess.run("wsl -u root"), capture_output=True)
+elif "WIN-02-ASUS" in output.stdout.decode('utf-8').split():
+    print(subprocess.run("wsl apt-get update && apt-get upgrade -y"), capture_output=True)
+else:
+    print('hostname not found!')
