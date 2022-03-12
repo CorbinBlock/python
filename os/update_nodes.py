1e048c8448e18383e1f876c330798280d6f1331f
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-12 14:33:29 -0600

    initial

--- os/update_nodes.py
+++ os/update_nodes.py
@@ -23,7 +23,7 @@ shell = "/bin/bash"
 
 wsl_command = 'bash -c "apt-get update && apt-get upgrade -y && apt-get install vim git git-lfs python3 python3-pip python3-venv openjdk-11-jdk curl -y && cd /git/python && git pull --no-rebase"'
 
-wsl_command_ssh = 'ssh root@172.31.23.178 "cd /git/python/os; python3 update_nodes.py"'
+wsl_command_ssh = 'ssh root@172.25.144.1 "cd /git/python/os; python3 update_nodes.py"'
 
 
 
