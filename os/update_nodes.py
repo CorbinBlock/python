8a55f00bb8b44fafd7e01d3239a48b70772f16d2
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-23 17:03:29 -0500

    changed to .dev dirs for source code

--- os/update_nodes.py
+++ os/update_nodes.py
@@ -30,7 +30,7 @@ powershell = "pwsh.exe"
 
 shell = "/bin/bash"
 
-wsl_command = 'bash -c "apt-get update && apt-get upgrade -y && apt-get install vim git git-lfs python3 python3-pip python3-venv openjdk-11-jdk curl -y && cd /git/python && git pull --no-rebase && cd /git/node && git pull --no-rebase"'
+wsl_command = 'bash -c "apt-get update && apt-get upgrade -y && apt-get install vim git git-lfs python3 python3-pip python3-venv openjdk-11-jdk curl -y && cd ~/.dev/python && git pull --no-rebase && cd ~/.dev/node && git pull --no-rebase"'
 
 wsl_command_ssh = 'ssh root@172.25.144.1 "cd /git/python/os; python3 update_nodes.py"'
 
