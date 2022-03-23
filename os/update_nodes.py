332489be39ad74f04bee93578e4a88f5e69161f3
CorbinBlock <williamcorbinblock@gmail.com>
2022-03-23 17:07:12 -0500

    fixed debian python update for root, must change for users account

--- os/update_nodes.py
+++ os/update_nodes.py
@@ -8,8 +8,8 @@ hostname
 debian_command = """
 apt-get update && apt-get upgrade -y
 apt-get install vim git git-lfs python3 python3-pip python3-venv openjdk-11-jdk curl -y
-cd /git/python && git pull --no-rebase
-cd /git/node && git pull --no-rebase
+cd ~/.dev/python && git pull --no-rebase
+cd ~/.dev/node && git pull --no-rebase
 """
 
 fedora_command = """
